a348e1d362609d0906e1037be0f2cd93f9252e7f
Kartheek-annapareddy <kartheekanapareddy@gmail.com>
2024-12-02 17:43:42 +0530

    third commit

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -8,7 +8,7 @@ import speech from '../../assets/logos/text to speech.png'
 
 function Projects() {
 
-  var projectdetails = [{ title: "Calculator App", img:calc, description: "simple calculator which can perform some of the basic  mathematical operations" }, { title: "Weather", img:weather, description: "Created Weather application using basic UI & Integreted weather API using Axios library " }, { title: "Stop Watch", img:stopwatch, description: "" }, { title: "Text to Speech Converter", img:speech, description: "" }]
+  var projectdetails = [{ title: "Calculator App", img:calc, description: "simple calculator which can perform some of the basic  mathematical operations",sourcecode:'#',live:'#' }, { title: "Weather", img:weather, description: "Created Weather application using basic UI & Integreted weather API using Axios library ",sourcecode:'https://github.com/Kartheek-annapareddy/WeatherApp',live:'https://aweatherapplication-ak.netlify.app/' }, { title: "Stop Watch", img:stopwatch, description: "" }, { title: "Text to Speech Converter", img:speech, description: "" }]
   return (
     // <div className='project-Container' id="project-container">
     //     <h3 className='project'>Projects </h3><br/><hr className='project-heading-line'/>
@@ -56,8 +56,8 @@ function Projects() {
                         <h3 className='project-name'>{ele.title}</h3>
                         <p className='project-description'>{ele.description}</p>
                         <div className='project-btn-container'>
-                          <button className='project-button'>Source code</button>
-                          <button className='project-button'>Live </button>
+                          <a href={ele.sourcecode} target='_blank'><button className='project-button'>Source code</button></a>
+                          <a href={ele.live} target='_blank'><button className='project-button'>Live </button></a>
                         </div>
                       </div>
                     </div>
